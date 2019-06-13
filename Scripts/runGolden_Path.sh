104c5a87c150942ddc3367372ed58831a7ef7274
Nico <transient@duvenage.com>
2019-06-13 14:46:26 +0200

    QA and Regression Scripting Update
    Changed the location of the environment file to the updated version and added documentation.

--- Scripts/runGolden_Path.sh
+++ Scripts/runGolden_Path.sh
@@ -3,4 +3,4 @@
 # Script to execute default scripts
 #
 
-sh ./testMojaloop.sh "https://raw.githubusercontent.com/mojaloop/postman/master/Golden_Path.postman_collection.json" "https://raw.githubusercontent.com/mojaloop/postman/master/environments/Mojaloop-DEV0.postman_environment.json" "nico@modusbox.com,sam@modusbox.com,henk.kodde@modusbox.com,sridevi.miriyala@modusbox.com" 1 > runGolden_Path.log
+sh ./testMojaloop.sh "https://raw.githubusercontent.com/mojaloop/postman/master/Golden_Path.postman_collection.json" "https://raw.githubusercontent.com/mojaloop/deploy-config/deploy/PI6.4/postmanEnvironments/Mojaloop-DEV0.postman_environment.json?token=ACG3PR3Z36U23XIF3YV2X225BNWKG" "nico@modusbox.com,sam@modusbox.com,henk.kodde@modusbox.com,sridevi.miriyala@modusbox.com" 1 > runGolden_Path.log
